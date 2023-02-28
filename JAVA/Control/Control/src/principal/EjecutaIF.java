f1d6856816cddc23c30ca60735664e41640a881f
AlejandroNoHdez <nohbryan10@gmail.com>
2023-02-28 17:41:33 -0600

    Archivos correspondientes a estructuras de control en Java

---
+++ JAVA/Control/Control/src/principal/EjecutaIF.java
@@ -0,0 +1,16 @@
+package principal;
+
+import control.EstructuraIf;
+
+public class EjecutaIF 
+{
+	public static void main(String[] args) 
+	{
+		EstructuraIf ei = new EstructuraIf();
+		//ei.controlIf();
+		//ei.divisible();
+		//ei.compara();
+		//ei.numMayor();
+		ei.realNum();
+	}
+}
